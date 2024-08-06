9460ae77d740c6aef6a23d1d260780eb40da0425
Ananda0502 <marciaevelly7@gmail.com>
2024-08-06 09:17:43 -0300

    imagens

---
+++ pizzariaitb/src/shared/Pizzas.jsx
@@ -0,0 +1,12 @@
+import mussarela from "../assets/imagens/mussarela.png"
+import calabresa from "../assets/imagens/calabresa.png"
+import frango from "../assets/imagens/frango.png"
+import atum from "../assets/imagens/atum.png"
+import bacon from "../assets/imagens/bacon.png"
+
+const Pizzas = [
+    {
+        "id" : 1,
+        
+    }
+]
