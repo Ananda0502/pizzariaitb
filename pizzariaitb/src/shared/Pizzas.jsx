1474b006017ebe541c34799ecea478759b56531a
Ananda0502 <marciaevelly7@gmail.com>
2024-08-07 08:26:40 -0300

    tentando

--- pizzariaitb/src/shared/Pizzas.jsx
+++ pizzariaitb/src/shared/Pizzas.jsx
@@ -7,6 +7,43 @@ import bacon from "../assets/imagens/bacon.png"
 const Pizzas = [
     {
         "id" : 1,
+        "nome" : "Mussarela",
+        "descricao" : "Molho de tomate e mussarela.",
+        "imagem" : mussarela
+        
+    },
+
+    {
+        "id" : 2,
+        "nome" : "Calabresa",
+        "descricao" : "Molho de tomate e calabresa.",
+        "imagem" : calabresa
+        
+    },
+
+    {
+        "id" : 3,
+        "nome" : "Frango com catupiry",
+        "descricao" : "Molho de tomate, frango e catupiry.",
+        "imagem" : frango
+        
+    },
+
+    {
+        "id" : 4,
+        "nome" : "Atum",
+        "descricao" : "Molho de tomate, mussarela e atum.",
+        "imagem" : atum
+        
+    },
+
+    {
+        "id" : 5,
+        "nome" : "Mussarela",
+        "descricao" : "Molho de tomate, mussarela e bacon.",
+        "imagem" : bacon
         
     }
 ]
+
+export default Pizzas;
